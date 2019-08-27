5ae8f3babd97b9c855b6a1ac39dc5d3cd53dae12
Gordey Doronin <g.doronin@gmail.com>
2019-08-27 20:53:42 +0700

    Remove XML conversion code

--- lib/taskmapper-pivotal.rb
+++ lib/taskmapper-pivotal.rb
@@ -2,22 +2,6 @@ require 'taskmapper'
 
 require 'pivotal/pivotal-api'
 
-# Monkey patch for backward compatibility issue with type cast on xml response
-class Hash
-  class << self
-    alias_method :from_xml_original, :from_xml
-
-    def from_xml(xml)
-      scrubbed = scrub_attributes(xml)
-      from_xml_original(scrubbed)
-    end
-
-    def scrub_attributes(xml)
-      xml.gsub(/<stories.*>/, "<stories type=\"array\">")
-    end
-  end
-end
-
 require 'provider/pivotal'
 require 'provider/ticket'
 require 'provider/project'
